274c13ad20337a1cadc795e93c2362f5b839da5b
unknown <>
2017-06-12 22:24:50 +0900

    add fake form

--- src/ViewM1Main.jsx
+++ src/ViewM1Main.jsx
@@ -1,6 +1,29 @@
 import React, { Component } from 'react';
+import KeyboardedInput from 'react-touch-screen-keyboard';
+import 'react-touch-screen-keyboard/src/Keyboard.css';
 
 export default class ViewM1Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        showLogin: false,
+        value: ''
+    };
+    this.handleOnChange = this.handleOnChange.bind(this);
+    this.onLogin = this.onLogin.bind(this);
+    this.onLoginFormToggle = this.onLoginFormToggle.bind(this);
+  }
+  handleOnChange(){
+
+  }
+  onLoginFormToggle() {
+    this.setState({
+      showLogin: !this.state.showLogin
+    });
+  }
+  onLogin() {
+    this.props.history.push('/m1/run');
+  }
   render() {
     return (
       <div>
@@ -24,34 +47,81 @@ export default class ViewM1Main extends Component {
           >
             <img src="/img/main_title1.png" alt="main_title1.png" />
           </div>
-          <div
-            style={{
-              position: 'absolute',
-              width: '100%',
-              zIndex: '9',
-              bottom: '105px'
-            }}
-          >
+          
+          {this.state.showLogin ?
             <div
               style={{
-                width: '667px',
-                height: '302px',
-                margin: '0 auto',
-                background: 'url(/img/v1-index-loginBox.png)',
-                paddingTop: '112px'
+                position: 'absolute',
+                width: '100%',
+                zIndex: '9',
+                bottom: '105px'
               }}
             >
               <div
-                className="login-form"                
                 style={{
-                  margin: '0px auto 0'
+                  width: '667px',
+                  height: '302px',
+                  margin: '0 auto',
+                  background: 'url(/img/login_box.png)',
+                  paddingTop: '112px',
+                  position: 'relative'
                 }}
+                
               >
-                <input type="password" className="login-pw" />
-                <input type="button" className="login-submit" value="Check in" />
+                <div
+                  onClick={this.onLoginFormToggle}
+                  style={{
+                    position: 'absolute',
+                    top: '0px',
+                    left: '0px',
+                    width: '100%',
+                    height: '86px'
+                  }}
+                >
+                  &nbsp;
+                </div>
+                <div
+                  className="login-form"                
+                  style={{
+                    margin: '0px auto 0'
+                  }}
+                  
+                >
+                  <KeyboardedInput
+                    enabled
+                    className="login-pw"
+                    type="password"
+                    defaultKeyboard="us"
+                    onChange={this.handleOnChange}
+                  /> 
+                  {/*<input type="password" className="login-pw" />*/}
+                  <input type="button" className="login-submit" value="Check in" onClick={this.onLogin} />
+                </div>
               </div>
             </div>
-          </div>
+            :
+            <div>
+              <div
+                style={{
+                  position: 'absolute',
+                  width: '100%',
+                  zIndex: '9',
+                  bottom: '120px'
+                }}
+                onClick={this.onLoginFormToggle}
+              >
+                <div
+                  style={{
+                    width: '607px',
+                    height: '89px',
+                    margin: '0 auto',
+                    background: 'url(/img/login_before.png)',
+                  }}
+                />
+              </div>
+            </div> 
+          }
+        
         </div>        
       </div>
     );
