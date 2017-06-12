b0e7372852c3d06c21c029fc3d3eec7325ed4a69
vinecgp <vinecgp@gmail.com>
2017-06-13 00:19:11 +0900

    push

--- src/ViewM1Main.jsx
+++ src/ViewM1Main.jsx
@@ -37,12 +37,14 @@ export default class ViewM1Main extends Component {
             position: 'relative'
           }}
         >
+        {/*
           <video
             autoPlay="true"
             src="/video/main.mp4"
             style={{
             }}
           />
+          */}
           <div
             style={{
               textAlign: 'center',
