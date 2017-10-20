07574cd4c1d319a8acea2f14c814fe1593d1d4a4
jjailbird <jjailbird@gmail.com>
2017-10-20 11:34:29 +0900

    Adjust LaserBarChart css

--- src/components/LaserBarChart.jsx
+++ src/components/LaserBarChart.jsx
@@ -28,69 +28,7 @@ export default class LaserBarChart extends Component {
     
     barMax = max ? parseInt(max) : 0;
     lineValueShift = shift ? parseInt(shift) : 0;
-    /*
-    switch(dataName) {
-      case 'lxA':
-        // HSI_001 
-        lineValueShift = 0;
-        barMax = 500;
-        break;
-      case 'lxS':
-        lineValueShift = 0;
-        barMax = 500;
-        break;
-      case 'ly1A':
-        // HSI_002
-        lineValueShift = 0;
-        barMax = 500;
-        break;
-      case 'ly1S':
-        lineValueShift = 0;
-        barMax = 500;
-        break;
-      case 'ly2A': 
-        // HSI_003, HSI_023
-        lineValueShift = 20;
-        barMax = 40;
-        break;
-      case 'ly2S':
-        lineValueShift = 10;
-        barMax = 20;
-        break;
-      case 'rxA':
-        lineValueShift = 0;
-        barMax = 500;
-        break;
-      case 'rxS':
-        lineValueShift = 0;
-        barMax = 500;
-        break;
-      case 'ry1A':
-        lineValueShift = 0;
-        barMax = 500;
-        break;
-      case 'ry1S':
-        lineValueShift = 0;
-        barMax = 500;
-        break;
-      case 'ry2A':
-        lineValueShift = 10;
-        barMax = 20;
-        break;
-      case 'ry2S':
-        lineValueShift = 10;
-        barMax = 20;
-        break;
-      case 'gA':
-        lineValueShift = 5;
-        barMax = 10;
-        break;
-      case 'gS':
-        lineValueShift = 5;
-        barMax = 10;
-        break;
-    }
-    */
+   
     if(data != undefined) {
       value = data;
       barValue = value + lineValueShift;
@@ -104,7 +42,8 @@ export default class LaserBarChart extends Component {
           <div 
             className="BarGraphFill" 
             style={{
-              height: `${barPx}`,
+              // height: `${barPx}`,
+              height: `${barPercent}%`,
               background: `${barColorReplace}`
             }}
           ></div>
